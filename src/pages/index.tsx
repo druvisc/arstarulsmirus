e7c88fc1fd0eaaf97160c5c6b3bbd6228e557afe
Druvis Cukurs <druvis.cukurs@gmail.com>
2022-09-23 17:17:23 +0300

    Remove commented code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,53 +4,14 @@ import { HeroSection } from "../components/HeroSection";
 import { ConcertSection } from "../components/ConcertSection";
 import { SocialSection } from "../components/SocialSection";
 import { ContactSection } from "../components/ContactSection";
-import { Logo } from "../components/svg/Logo";
-import { useEffect, useRef } from "react";
 
 const HomePage: NextPage = () => {
-  // const anchorRef = useRef<HTMLDivElement>(null);
-
-  // const logoRef = useRef<SVGSVGElement>(null);
-
-  // useEffect(() => {
-  //   const anchor = anchorRef.current;
-  //   const logo = logoRef.current;
-
-  //   if (!anchor || !logo) return;
-
-  //   const handler = () => {
-  //     const scrollTop = window.scrollY;
-  //     console.log({ scrollTop });
-
-  //     const offsetTop =
-  //       anchor.getBoundingClientRect().top +
-  //       (document.scrollingElement || document.documentElement).scrollTop;
-
-  //     // const fixed = logo.classList.contains("a-logo-fixed");
-
-  //     // if (scrollTop > offsetTop) {
-  //     //   if (!fixed) logo.classList.add("a-logo-fixed");
-  //     // } else {
-  //     //   if (fixed) logo.classList.remove("a-logo-fixed");
-  //     // }
-  //   };
-
-  //   window.addEventListener("scroll", handler);
-
-  //   return () => window.removeEventListener("scroll", handler);
-  // });
-
   return (
     <>
       <HeroSection />
-
-      {/* <div ref={anchorRef}></div> */}
-
       <ConcertSection />
       <SocialSection />
       <ContactSection />
-
-      {/* <Logo ref={logoRef} className="absolute right-[52px]" /> */}
     </>
   );
 };
